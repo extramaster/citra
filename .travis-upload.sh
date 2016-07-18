bc8ef488975567f8739eeda303c2a251a20d3341
Andy Tran <extramaster+github@live.com.au>
2016-07-18 18:21:59 +1000

    Mac OS X build test

--- .travis-upload.sh
+++ .travis-upload.sh
@@ -36,4 +36,38 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 
     # Personal build to check out what's going on with Travis-CI and Mac OS X (not MacOS, that isn't out yet).
     # lftp -c "open -u citra-builds,$BUILD_PASSWORD sftp://builds.citra-emu.org; set sftp:auto-confirm yes; put -O '$UPLOAD_DIR' '$ARCHIVE_NAME'"
+    # https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
+    TARGET_BRANCH="builds"
+    
+    REPO=`git config remote.origin.url`
+    SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+    SHA=`git rev-parse --verify HEAD`
+
+    # Clone the existing gh-pages for this repo into out/
+    # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+    git clone $REPO out
+    cd out
+    git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+    cd ..
+    rm -rf out/**/* || exit 0
+    mv $ARCHIVE_NAME out
+    cd out
+    git config user.name "Travis CI"
+    git config user.email "spam@example.com"
+    git add .
+    git commit -m "Deploy to Build: ${SHA}"
+    
+    # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+    ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+    ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+    ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+    ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+    openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+    chmod 600 deploy_key
+    eval `ssh-agent -s`
+    ssh-add deploy_key
+
+    # Now that we're all set up, we can push.
+    git push $SSH_REPO $TARGET_BRANCH
+    
 fi
