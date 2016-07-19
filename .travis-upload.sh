38939fc257e7a805408171064918f2e7273322f0
Andy Tran <extramaster+github@live.com.au>
2016-07-19 13:33:42 +1000

    Travis: Clean up lines…

--- .travis-upload.sh
+++ .travis-upload.sh
@@ -32,7 +32,8 @@ if [ "$TRAVIS_BRANCH" != "builds" ]; then
         # Hai! These will be eventually pushed into a PR... As soon as I can verify that travis builds it on my own personal copy...
 
         # Make the changes to make the citra-qt app standalone (i.e. not dependent on the current brew installation).
-        # To do this, the absolute references to each and every QT framework must be re-written to point to the local frameworks (in the Contents/Frameworks folder).
+        # To do this, the absolute references to each and every QT framework must be re-written to point to the local frameworks
+        # (in the Contents/Frameworks folder).
         # The "install_name_tool" is used to do so.
 
         # Coreutils is a hack to coerce Homebrew to point to the absolute Cellar path (symlink dereferenced). i.e:
@@ -46,39 +47,57 @@ if [ "$TRAVIS_BRANCH" != "builds" ]; then
         BREW_PATH=$(brew --prefix)
         QT_VERSION_NUM=5
 
-        $BREW_PATH/opt/qt5/bin/macdeployqt "${REV_NAME_ALT}citra-qt.app" -executable="${REV_NAME_ALT}citra-qt.app/Contents/MacOS/citra-qt"
+        $BREW_PATH/opt/qt5/bin/macdeployqt "${REV_NAME_ALT}citra-qt.app" \
+            -executable="${REV_NAME_ALT}citra-qt.app/Contents/MacOS/citra-qt"
 
         # These are the files that macdeployqt packed into Contents/Frameworks/ - we don't want those, so we replace them.
         declare -a macos_libs=("QtCore" "QtWidgets" "QtGui" "QtOpenGL" "QtPrintSupport")
 
         for macos_lib in "${macos_libs[@]}"
         do
-               # Replace macdeployqt versions of the Frameworks with our own (from /usr/local/opt/qt5/lib/)
-               cp "$BREW_PATH/opt/qt5/lib/$macos_lib.framework/Versions/$QT_VERSION_NUM/$macos_lib" "${REV_NAME_ALT}citra-qt.app/Contents/Frameworks/$macos_lib.framework/Versions/$QT_VERSION_NUM/$macos_lib"
+            SC_FRAMEWORK_PART=$macos_lib.framework/Versions/$QT_VERSION_NUM/$macos_lib
+            # Replace macdeployqt versions of the Frameworks with our own (from /usr/local/opt/qt5/lib/)
+            cp "$BREW_PATH/opt/qt5/lib/$SC_FRAMEWORK_PART" "${REV_NAME_ALT}citra-qt.app/Contents/Frameworks/$SC_FRAMEWORK_PART"
 
-               # Replace references within the embedded Framework files with "internal" versions.
-               for macos_lib2 in "${macos_libs[@]}"
-               do
-                   # Since brew references both the non-symlinked and symlink paths of QT5, it needs to be duplicated.
-                   # /usr/local/Cellar/qt5/5.6.1-1/lib and /usr/local/opt/qt5/lib both resolve to the same files. So the two lines are effectively duplicates when resolved as a path, but as strings, they aren't.
-                   install_name_tool -change $QT_BREWS_PATH/lib/$macos_lib2.framework/Versions/$QT_VERSION_NUM/$macos_lib2 @executable_path/../Frameworks/$macos_lib2.framework/Versions/$QT_VERSION_NUM/$macos_lib2 "${REV_NAME_ALT}citra-qt.app/Contents/Frameworks/$macos_lib.framework/Versions/$QT_VERSION_NUM/$macos_lib"
-                   install_name_tool -change "$BREW_PATH/opt/qt5/lib/$macos_lib2.framework/Versions/$QT_VERSION_NUM/$macos_lib2" @executable_path/../Frameworks/$macos_lib2.framework/Versions/$QT_VERSION_NUM/$macos_lib2 "${REV_NAME_ALT}citra-qt.app/Contents/Frameworks/$macos_lib.framework/Versions/$QT_VERSION_NUM/$macos_lib"
-               done
+            # Replace references within the embedded Framework files with "internal" versions.
+            for macos_lib2 in "${macos_libs[@]}"
+            do
+                # Since brew references both the non-symlinked and symlink paths of QT5, it needs to be duplicated.
+                # /usr/local/Cellar/qt5/5.6.1-1/lib and /usr/local/opt/qt5/lib both resolve to the same files.
+                # So the two lines below are effectively duplicates when resolved as a path, but as strings, they aren't.
+                RM_FRAMEWORK_PART=$macos_lib2.framework/Versions/$QT_VERSION_NUM/$macos_lib2
+                install_name_tool -change \
+                    $QT_BREWS_PATH/lib/$RM_FRAMEWORK_PART \
+                    @executable_path/../Frameworks/$RM_FRAMEWORK_PART \
+                    "${REV_NAME_ALT}citra-qt.app/Contents/Frameworks/$SC_FRAMEWORK_PART"
+                install_name_tool -change \
+                    "$BREW_PATH/opt/qt5/lib/$RM_FRAMEWORK_PART" \
+                    @executable_path/../Frameworks/$RM_FRAMEWORK_PART \
+                    "${REV_NAME_ALT}citra-qt.app/Contents/Frameworks/$SC_FRAMEWORK_PART"
+            done
         done
 
-        # Handles the "This application failed to start because it could not find or load the Qt platform plugin "cocoa"" which masks itself as the ""
-        # Which manifests itself as "Exception Type:        EXC_CRASH (SIGABRT) Exception Codes:       0x0000000000000000, 0x0000000000000000 Exception Note:        EXC_CORPSE_NOTIFY"
+        # Handles `This application failed to start because it could not find or load the Qt platform plugin "cocoa"`
+        # Which manifests itself as:
+        # "Exception Type: EXC_CRASH (SIGABRT) | Exception Codes: 0x0000000000000000, 0x0000000000000000 | Exception Note: EXC_CORPSE_NOTIFY"
         # There may be more dylibs needed to be fixed...
         declare -a macos_plugins=("Plugins/platforms/libqcocoa.dylib")
 
         for macos_lib in "${macos_plugins[@]}"
         do
-               install_name_tool -id @executable_path/../$macos_lib "${REV_NAME_ALT}citra-qt.app/Contents/$macos_lib"
-               for macos_lib2 in "${macos_libs[@]}"
-               do
-                   install_name_tool -change $QT_BREWS_PATH/lib/$macos_lib2.framework/Versions/$QT_VERSION_NUM/$macos_lib2 @executable_path/../Frameworks/$macos_lib2.framework/Versions/$QT_VERSION_NUM/$macos_lib2 "${REV_NAME_ALT}citra-qt.app/Contents/$macos_lib"
-                   install_name_tool -change "$BREW_PATH/opt/qt5/lib/$macos_lib2.framework/Versions/$QT_VERSION_NUM/$macos_lib2" @executable_path/../Frameworks/$macos_lib2.framework/Versions/$QT_VERSION_NUM/$macos_lib2 "${REV_NAME_ALT}citra-qt.app/Contents/$macos_lib"
-               done
+            install_name_tool -id @executable_path/../$macos_lib "${REV_NAME_ALT}citra-qt.app/Contents/$macos_lib"
+            for macos_lib2 in "${macos_libs[@]}"
+            do
+                RM_FRAMEWORK_PART=$macos_lib2.framework/Versions/$QT_VERSION_NUM/$macos_lib2
+                install_name_tool -change \
+                    $QT_BREWS_PATH/lib/$RM_FRAMEWORK_PART \
+                    @executable_path/../Frameworks/$RM_FRAMEWORK_PART \
+                    "${REV_NAME_ALT}citra-qt.app/Contents/$macos_lib"
+                install_name_tool -change \
+                    "$BREW_PATH/opt/qt5/lib/$RM_FRAMEWORK_PART" \
+                    @executable_path/../Frameworks/$RM_FRAMEWORK_PART \
+                    "${REV_NAME_ALT}citra-qt.app/Contents/$macos_lib"
+            done
         done
 
         for macos_lib in "${macos_libs[@]}"
